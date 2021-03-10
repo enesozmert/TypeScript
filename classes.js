08427a61621332ccca2a40c0a96ad4c76fc735e2
enesozmert <en-es111@hotmail.com>
2021-03-10 16:30:38 +0300

    new Work

--- classes.js
+++ classes.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Ev = /** @class */ (function () {
     function Ev(odaSayisi, pencereSayisi, kat) {
         this._odasayisi = odaSayisi;
@@ -12,3 +27,49 @@ var Ev = /** @class */ (function () {
 var ev = new Ev(1, 2, 3);
 ev.yemekYe();
 console.log(ev._kat);
+var Kisi = /** @class */ (function () {
+    function Kisi() {
+    }
+    Object.defineProperty(Kisi.prototype, "isim", {
+        get: function () {
+            return "Sayın : " + this._isimnewnew;
+        },
+        set: function (ad) {
+            this._isimnewnew = ad;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Kisi.prototype.kaydet = function () {
+        console.log("Kisi kaydedildi");
+    };
+    return Kisi;
+}());
+var Mustere = /** @class */ (function (_super) {
+    __extends(Mustere, _super);
+    function Mustere() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Mustere.prototype.satisYap = function () {
+        console.log("satiş yapıldı");
+    };
+    return Mustere;
+}(Kisi));
+var Personel = /** @class */ (function (_super) {
+    __extends(Personel, _super);
+    function Personel() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Personel.prototype.maasOde = function () {
+        console.log("maaş ödendi");
+    };
+    return Personel;
+}(Kisi));
+var musteri = new Mustere();
+musteri.isim = "Engin";
+console.log(musteri.isim);
+musteri.kaydet();
+musteri.satisYap();
+var personel = new Personel();
+personel.kaydet();
+personel.maasOde();
--- classes.ts
+++ classes.ts
@@ -1,7 +1,7 @@
 class Ev {
-    _odasayisi: number;
-    _pencereSayisi: number;
-    _kat: number;
+    public _odasayisi: number;
+    private _pencereSayisi: number; //ulaşılamaz
+    public _kat: number;
     constructor(odaSayisi: number, pencereSayisi: number, kat: number) {
         this._odasayisi = odaSayisi
         this._pencereSayisi = pencereSayisi
@@ -14,4 +14,36 @@ class Ev {
 let ev = new Ev(1, 2, 3)
 ev.yemekYe();
 console.log(ev._kat)
-console.log(ev._kat)
+class Kisi {
+    protected _isim: string //inherit edilenlerden ulaşılabilir
+    private _isimnew: string
+    private _isimnewnew: string
+    get isim(): string {
+        return "Sayın : " + this._isimnewnew
+    }
+    set isim(ad: string) {
+        this._isimnewnew = ad
+    }
+    kaydet(): void {
+        console.log("Kisi kaydedildi")
+    }
+}
+class Mustere extends Kisi {
+    satisYap(): void {
+        console.log("satiş yapıldı")
+    }
+}
+class Personel extends Kisi {
+    maasOde() {
+        console.log("maaş ödendi")
+    }
+}
+let musteri = new Mustere()
+musteri.isim = "Engin";
+console.log(musteri.isim)
+musteri.kaydet()
+musteri.satisYap()
+
+let personel = new Personel()
+personel.kaydet()
+personel.maasOde()
